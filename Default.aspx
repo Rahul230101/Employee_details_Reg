247b7d4199a5cf7a8b896d6c8c0b6a7a33853917
Rahul Rane <93042279+Rahul230101@users.noreply.github.com>
2022-10-14 19:33:15 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Employee_details._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <br />
+    <div align="center" style="font-size: x-large"><strong>Employee Details</strong></div><br />
+    <br />
+    <div class="col-lg-8 col-lg-offset-2">
+        <table class="table table-striped">
+            <tr>
+                <th style="font-size: medium; width: 512px;">Name</th>
+                <td style="width: 864px">
+                    <asp:TextBox CssClass="form-control" runat="server" ID="txtName" style="margin-left: 0"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <th style="font-size: medium; width: 512px;">Age</th>
+                <td style="width: 864px">
+                    <asp:TextBox runat="server" CssClass="form-control" ID="txtAge"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <th style="font-size: medium; width: 512px;">Gender</th>
+                <td style="width: 864px">
+                    <asp:DropDownList CssClass="btn btn-default dropdown-toggle" runat="server" ID="ddlgender" DataSourceID="SqlDataSource1" DataTextField="Gender" DataValueField="Gender_id" >
+                    </asp:DropDownList>
+                    
+                     
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:DBCS %>" SelectCommand="SELECT [Gender_id], [Gender] FROM [gender]"></asp:SqlDataSource>
+                    
+                     
+                </td>
+            </tr>
+            
+            <tr>
+                <td style="width: 512px">
+                    <asp:Button CssClass="btn-success" runat="server" ID="btnInsert" Text="Insert" OnClick="btnInsert_Click1" Height="38px" Width="117px" /></td>
+                <td style="width: 864px">
+                    <asp:Button CssClass="btn-info" runat="server" ID="btnUpdate" Text="Update" OnClick="btnUpdate_Click" Height="38px" Width="119px" />
+                    </td>
+            </tr>
+        </table>
+    </div>
+    <br />
+    <br />
+    <div>
+        <asp:GridView runat="server" OnRowDeleting="Gridview1_RowDeleting" ID="Gridview1" CssClass="table" DataKeyNames="Age" AutoGenerateColumns="false" >
+            <Columns>
+                <asp:BoundField HeaderText="Employee_name" DataField="Name"/>
+                <asp:BoundField HeaderText="Employee_age" DataField="Age"/>
+                <asp:BoundField HeaderText="Gender_ID" DataField="Gender"/>
+                <asp:TemplateField HeaderText="Action">
+                    <ItemTemplate>
+                        <asp:Button CssClass="btn-danger" runat="server" ID="btndelete" Text="Delete" Height="38px" Width="119px" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Configuration;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using System.Data;
+using System.Security.Policy;
+
+namespace Employee_details
+{
+    public partial class _Default : Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Load_record();
+            }
+        }
+
+        public void Load_record()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("Select * from Employee_info", con);
+                DataSet ds = new DataSet();
+                con.Open();
+                sda.Fill(ds);
+                Gridview1.DataSource = ds;
+                Gridview1.DataBind();
+
+            }
+        }
+        protected void btnInsert_Click1(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO Employee_info VALUES ('" + txtName.Text + "', '" + Convert.ToInt32(txtAge.Text) + "', '" + ddlgender.Text + "')", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data has been Inserted')", true);
+                Load_record();
+
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Update Employee_info set Name = '" + txtName.Text + "', Age = '" + Convert.ToInt32(txtAge.Text) + "', Gender = '" + ddlgender.Text + "'  Where Name = '" + txtName.Text + "' ", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data has been Updated')", true);
+                Load_record();
+            }
+        }
+
+
+        protected void Gridview1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Delete From Employee_info Where Age = '" + Convert.ToInt32(Gridview1.DataKeys[e.RowIndex].Value) + "' ", con);
+                con.Open();
+                int row = cmd.ExecuteNonQuery();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data has been Deleted')",true);
+                if (row > 0)
+                {
+                    Load_record();
+                }
+            }
+
+        }
+    }
+}
+    
+
+//Age = '" + Convert.ToInt32(txtAge.Text) + "'
+
